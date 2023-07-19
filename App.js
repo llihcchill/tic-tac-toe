0e31d3f7198641f8f05a7ac163d25de2fc8550c2
llihcchill <125551072+llihcchill@users.noreply.github.com>
2023-07-19 18:42:02 +1000

    Update App.js

--- App.js
+++ App.js
@@ -1,20 +1,25 @@
-export default function Square() {
+export default function Board() {
   return (
     // a function cannot return multiple JSX elements at once
     // in order to actually return multiple, you can wrap all of the
     // elements into a fragment and render them all in one
     <>
-      <button className="square">X</button>
-      <button className="square">X</button>
-      <button className="square">X</button>
-
-      <button className="square">X</button>
-      <button className="square">X</button>
-      <button className="square">X</button>
-
-      <button className="square">X</button>
-      <button className="square">X</button>
-      <button className="square">X</button>
+      <div className="board-row">
+      // className in JSX is class="" in regular HTML
+        <button className="square">1</button>
+        <button className="square">2</button>
+        <button className="square">3</button>
+      </div>
+      <div className="board-row">
+        <button className="square">4</button>
+        <button className="square">5</button>
+        <button className="square">6</button>
+      </div>
+      <div className="board-row">
+        <button className="square">7</button>
+        <button className="square">8</button>
+        <button className="square">9</button>
+      </div>
     </>
   );
 }
