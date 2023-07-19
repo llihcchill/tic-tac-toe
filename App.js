18cfaa122732c05eea591d016161f9c9430fba7d
llihcchill <125551072+llihcchill@users.noreply.github.com>
2023-07-19 18:18:03 +1000

    Create App.js

---
+++ App.js
@@ -0,0 +1,6 @@
+export default function Square() {
+  return <button className="square">X</button>;
+}
+
+// export means that this function can be used anywhere
+// default means that this function is the main one
