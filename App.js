6ebfec0f55dac881e37faba54a753274ba29f298
llihcchill <125551072+llihcchill@users.noreply.github.com>
2023-07-19 18:35:40 +1000

    Update App.js

--- App.js
+++ App.js
@@ -1,6 +1,23 @@
 export default function Square() {
-  return <button className="square">X</button>;
+  return (
+    // a function cannot return multiple JSX elements at once
+    // in order to actually return multiple, you can wrap all of the
+    // elements into a fragment and render them all in one
+    <>
+      <button className="square">X</button>
+      <button className="square">X</button>
+      <button className="square">X</button>
+
+      <button className="square">X</button>
+      <button className="square">X</button>
+      <button className="square">X</button>
+
+      <button className="square">X</button>
+      <button className="square">X</button>
+      <button className="square">X</button>
+    </>
+  );
 }
 
 // export means that this function can be used anywhere
-// default means that this function is the main one
+// default means that this function is the main one in the file
