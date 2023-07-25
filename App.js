5568631cb0692cbdfd341529e1110bfa01df3e1d
llihcchill <125551072+llihcchill@users.noreply.github.com>
2023-07-25 18:24:32 +1000

    Update App.js

--- App.js
+++ App.js
@@ -1,26 +1,21 @@
 import { useState } from "react";
 // this is a special function that practically lets react 'remember' things
 
-function Square() {
-  const [value, setValue] = useState(null);
+function Square({ value, onSquareClick }) {
+  // const [value, setValue] = useState(null);
   // value = the value and setValue = change the value of value
   // the null in useState(null) is the default value of value
 
-  function handleClick() {
-    setValue("X");
-    // setValue is not a library function, it is the function that
-    // is used in the useState constant value and it is changing
-    // value to be equal to X and not 1 then 2 then 3 etc.
-  }
+  //function handleClick() {
+  //setValue("X");
+  // setValue is not a library function, it is the function that
+  // is used in the useState constant value and it is changing
+  // value to be equal to X and not 1 then 2 then 3 etc.
+  //}
   // function doesn't contain export so it will not be rendered
   // and is used for the client-side logic
   return (
-    <button
-      className="square"
-      onClick={
-        handleClick
-      } /* onClick and related functions are supported with functions */
-    >
+    <button className="square" onClick={onSquareClick}>
       {value}
     </button>
   );
@@ -43,6 +38,15 @@ export default function Board() {
   // now squares can be used to display the squares' state whereas
   // here setSquares can be used to set each square by their array place
 
+  function handleClick(i) {
+    const nextSquares = squares.slice();
+    // .slice() function copies the array practically
+    nextSquares[i] = "X";
+    setSquares(nextSquares);
+    // remember setSquares is a function from the useState function
+    // this is also where all the game logic will go
+  }
+
   return (
     // a function cannot return multiple JSX elements at once
     // in order to actually return multiple, you can wrap all of the
@@ -57,19 +61,19 @@ export default function Board() {
     // function calls
     <>
       <div className="board-row">
-        <Square value={squares[0]} />
-        <Square value={squares[1]} />
-        <Square value={squares[2]} />
+        <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
+        <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
+        <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
       </div>
       <div className="board-row">
-        <Square value={squares[3]} />
-        <Square value={squares[4]} />
-        <Square value={squares[5]} />
+        <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
+        <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
+        <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
       </div>
       <div className="board-row">
-        <Square value={squares[6]} />
-        <Square value={squares[7]} />
-        <Square value={squares[8]} />
+        <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
+        <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
+        <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
       </div>
     </>
   );
