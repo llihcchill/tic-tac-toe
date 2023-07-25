114d83ae6f68a3950f15e9228c8e19ee5a20aa3c
llihcchill <125551072+llihcchill@users.noreply.github.com>
2023-07-25 17:56:05 +1000

    Update App.js

--- App.js
+++ App.js
@@ -30,6 +30,19 @@ export default function Board() {
   // export means that this function can be used anywhere
   // default means that this function is the main one in the file
 
+  // "To collect data from multiple children, or to have
+  // two child components communicate with each other, declare
+  // the shared state in their parent component instead.
+  // The parent component can pass that state back down to the
+  // children via props. This keeps the child components in sync
+  // with each other and with their parent."
+
+  // creates an array called squares and setSquares
+  const [squares, setSquares] = useState(Array(9).fill(null));
+
+  // now squares can be used to display the squares' state whereas
+  // here setSquares can be used to set each square by their array place
+
   return (
     // a function cannot return multiple JSX elements at once
     // in order to actually return multiple, you can wrap all of the
@@ -44,19 +57,19 @@ export default function Board() {
     // function calls
     <>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[0]} />
+        <Square value={squares[1]} />
+        <Square value={squares[2]} />
       </div>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[3]} />
+        <Square value={squares[4]} />
+        <Square value={squares[5]} />
       </div>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[6]} />
+        <Square value={squares[7]} />
+        <Square value={squares[8]} />
       </div>
     </>
   );
