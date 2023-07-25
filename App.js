0aa08d6ac2105d009abf5c19e357f25e62b002cb
llihcchill <125551072+llihcchill@users.noreply.github.com>
2023-07-25 19:13:48 +1000

    Update App.js

--- App.js
+++ App.js
@@ -83,6 +83,7 @@ export default function Board() {
     // to add data into the {value} prop, you can set it from the
     // function calls
     <>
+      <div className="status">{status}</div>
       <div className="board-row">
         <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
         <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
@@ -133,4 +134,3 @@ function calculateWinner(squares) {
   // if the game has not been won, it does nothing
   return null;
 }
-
